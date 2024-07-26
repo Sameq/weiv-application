b67c48c10dce2ec89eeb9ce2d9a3bba423a473c6
Hiego <hiegobarreto@gmail.com>
2024-07-26 13:56:08 -0300

    project scafolder

---
+++ src/main/java/br/com/ifba/weiv/WeivApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.weiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifba/weiv/WeivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.weiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
